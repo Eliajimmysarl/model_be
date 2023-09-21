3146b970a4c57a796bd7ccf54ec0342cc685ca2e
Jimmy ELIA <jimmy.elia@ejsarl.com>
2023-09-21 10:16:17 -0700

    fix import excel

--- entites/import_excel.php
+++ entites/import_excel.php
@@ -24,7 +24,9 @@
 
                             $sep=explode(";",$exp);
 
-                            $count = 0;
+                            $count = 0;^
+
+                            $data=array();
 
                             if($count > 0)
                                 {
@@ -55,6 +57,22 @@
                                     $stmt->bindParam(7, $optionsRadios); 
                 
                                     $stmt->execute();
+
+                                    $donnee[$i]['text']=$texte;
+
+                                    $donnee[$i]['select']=$selec;
+
+                                    $donnee[$i]['dates']=$dates;
+
+                                    $donnee[$i]['telephone']=$telephone;
+
+                                    $donnee[$i]['email']=$email;
+
+                                    $donnee[$i]['passwords']=$passwords;
+
+                                    $donnee[$i]['optionsRadios']=$optionsRadios;
+
+
                                 }
                         }
                     $last = $dbh->lastInsertId();
@@ -64,12 +82,16 @@
                             $data["code"]  = 400;
             
                             $data["message"]  = "Ressource not created";
+
+                            $data["excel"]  = $donnee;
                         }
                     else
                         {
                             $data["code"]  = 201;
             
                             $data["message"]  = "Ressource created";
+
+                            $data["excel"]  = $donnee;
                         }
                         
                     echo json_encode($data);
