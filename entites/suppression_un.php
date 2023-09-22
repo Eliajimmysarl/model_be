6dd560c6b5ff7da8aa61895091c8f038836af7af
yanmonga <yannick.monga@ejsarl.com>
2023-09-22 11:53:48 +0100

    update suppression

--- entites/suppression_un.php
+++ entites/suppression_un.php
@@ -1,34 +1,37 @@
 <?php
 
-try {
-   
-$dbh = new PDO('mysql:host=localhost;dbname='.$db_test, $user_test, $pass_test);
+    try {
+    
+            $dbh = new PDO('mysql:host=localhost;dbname='.$db_test, $user_test, $pass_test);
 
-$stmt = $dbh->prepare("DELETE FROM test WHERE id = :id");
+            $stmt = $dbh->prepare("DELETE FROM test WHERE id = :id");
 
-$stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
 
-$stmt->execute();
+            $stmt->execute();
 
-$stmt = $dbh->prepare("SELECT *FROM test  ORDER BY id");
-        
-$stmt->execute();
-        
-$datas = array();
-        
-        while($resultat=$stmt->fetch(PDO::FETCH_ASSOC)) 
-            {
-                $datas["code"]  = 200;
-                $datas['test'][]=$resultat;
-            }
-                
+            $stmt = $dbh->prepare("SELECT *FROM test  ORDER BY id");
+                    
+            $stmt->execute();
+                    
+            $datas = array();
+                    
+                    while($resultat=$stmt->fetch(PDO::FETCH_ASSOC)) 
+                        {
+                            $datas["code"]  = 200;
 
-echo json_encode( $datas);
+                            $datas['test'][]=$resultat;
+                        }
+                                
+            echo json_encode( $datas);
 
+        }
 
-    }
-catch (PDOException $e) {
-    print "Erreur !: " . $e->getMessage() . "<br/>";
-    die();
-}
+    catch (PDOException $e) 
+        {
+            print "Erreur !: " . $e->getMessage() . "<br/>";
+
+            die();
+        }
+           
 ?>
