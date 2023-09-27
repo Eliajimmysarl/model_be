79fe49de6ac8270392a565fc3f8e0996c1b37046
Jimmy ELIA <jimmy.elia@ejsarl.com>
2023-09-27 02:53:27 -0700

    fix modification un

--- entites/modification_un.php
+++ entites/modification_un.php
@@ -43,31 +43,7 @@
 
             $stmt->bindParam(9, $heureUpdate);
 
-            $stmt->execute();
-
-            $stmt = $dbh->prepare("SELECT *FROM test WHERE texte=? AND selec=? AND  dates=? AND telephone=? AND email=? AND passwords=? AND optionsRadios=? AND date_update=? AND  heure_update=?");
-            
-            $stmt->bindParam(1, $texte);
-
-            $stmt->bindParam(2, $selec);
-
-            $stmt->bindParam(3, $dates);
-
-            $stmt->bindParam(4, $telephone);
-
-            $stmt->bindParam(5, $email);
-
-            $stmt->bindParam(6, $passwords);
-            
-            $stmt->bindParam(7, $optionsRadios);
-
-            $stmt->bindParam(8, $id);
-
-            $stmt->bindParam(8, $dateUpdate);
-
-            $stmt->bindParam(9, $heureUpdate);
-
-            $stmt->execute();        
+            $stmt->execute();      
 
             $data["code"]  = 200;
 
