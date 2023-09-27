cade78d0dabbd9a693b67ff028bff7957cb37c67
Jimmy ELIA <jimmy.elia@ejsarl.com>
2023-09-27 00:58:55 -0700

    Fix recuperation avec jointure

--- entites/recuperation_avec_jointure.php
+++ entites/recuperation_avec_jointure.php
@@ -8,7 +8,7 @@
       {
          $dbh = new PDO('mysql:host=localhost;dbname='.$db_test, $user_test, $pass_test);
 
-         $stmt = $dbh->prepare("SELECT test.id, test.telephone, test.selec, test.dates, test.email, test.optionsRadios, selections.id, selections.nom, options.id, options.nom FROM `test` INNER JOIN selections ON test.selec=selections.id INNER JOIN options ON test.optionsRadios=options.id WHERE test.selec= ? AND test.optionsRadios= ? ");
+         $stmt = $dbh->prepare("SELECT test.id, test.telephone, test.selec, test.dates, test.email, test.optionsRadios, selections.id as selections_id, selections.nom as selections_nom, options.id as options_id, options.nom as options_nom FROM `test` INNER JOIN selections ON test.selec=selections.id INNER JOIN options ON test.optionsRadios=options.id WHERE test.selec= ? AND test.optionsRadios= ? ");
 
          $stmt->bindParam(1, $selec);
                      
